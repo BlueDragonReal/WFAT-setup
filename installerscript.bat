53e9e0d5416e955489951209883f708c1ed0895c
Blue <74019070+BlueDragonReal@users.noreply.github.com>
2023-08-20 03:40:20 +0200

    Update installerscript.bat

--- installerscript.bat
+++ installerscript.bat
@@ -15,21 +15,28 @@ if exist "%repo_dir%\Warframe-Algo-Trader" (
     git clone "%repo_url%"
 )
 
-REM Check for Node.js installation
-node -v > nul 2>&1
+REM Check for Python installation
+where python > nul 2>&1
 if %errorlevel% NEQ 0 (
-    echo Node.js is not installed. Installing Node.js...
-    start /wait "" "https://nodejs.org/dist/v14.17.6/node-v14.17.6-x64.msi"
-)
-
-REM Install Python 3.11 if not already installed
-python --version 2>nul
-if %errorlevel% NEQ 0 (
-    echo Installing Python 3.11...
+    echo Python is not installed. Installing Python 3.11...
     start /wait "" "https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe" /quiet
 )
 
-REM Continue with the rest of your script...
+REM Check for Git installation
+where git > nul 2>&1
+if %errorlevel% NEQ 0 (
+    echo Git is not installed. Please install Git and rerun this script.
+    pause
+    exit /b 1
+)
+
+REM Check for Node.js installation
+where node > nul 2>&1
+if %errorlevel% NEQ 0 (
+    echo Node.js is not installed. Please install Node.js and rerun this script.
+    pause
+    exit /b 1
+)
 
 REM Activate Python virtual environment
 cd "%repo_dir%\Warframe-Algo-Trader"
@@ -41,57 +48,10 @@ call venv\Scripts\activate
 REM Install required Python packages
 python -m pip install -r requirements.txt
 
-REM Gather user input
-set /p "ign=Enter your in-game name: "
-set /p "jwt_token=Enter your JWT token (including 'JWT' prefix if present): "
-
-REM Clean JWT token input (remove 'JWT' prefix if present)
-set "jwt_token=%jwt_token:JWT =%"
-
-REM Prompt for missing 'JWT' prefix if not provided
-if not "%jwt_token:~0,3%"=="JWT" (
-    echo JWT prefix is missing. Adding it to the token.
-    set "jwt_token=JWT %jwt_token%"
-)
-
-REM Offer platform choice and gather user input
-:platform_choice
-echo Choose your platform:
-echo [1] pc
-echo [2] ps4
-echo [3] xbox
-echo [4] switch
-set /p platform=Enter the platform number (1/2/3/4): 
-if "%platform%"=="1" set "platform=pc"
-if "%platform%"=="2" set "platform=ps4"
-if "%platform%"=="3" set "platform=xbox"
-if "%platform%"=="4" set "platform=switch"
-if not "%platform%"=="pc" if not "%platform%"=="ps4" if not "%platform%"=="xbox" if not "%platform%"=="switch" (
-    echo Invalid platform choice. Please select again.
-    goto platform_choice
-)
-
-REM Create config.json
-(
-    echo {
-    echo    "pushbutton_token": "",
-    echo    "pushbutton_device_iden": "",
-    echo    "inGameName": "%ign%",
-    echo    "wfm_jwt_token": "%jwt_token%",
-    echo    "runningLiveScraper": false,
-    echo    "runningStatisticsScraper": false,
-    echo    "runningWarframeScreenDetect": false,
-    echo    "platform": "%platform%"
-    echo }
-) > config.json
-
-echo Installation completed successfully!
-pause
+REM Rest of your script...
 
 REM Deactivate Python virtual environment
 deactivate
 
 REM Restore the previous environment
 endlocal
-
-REM BLUECAT WAS HERE
